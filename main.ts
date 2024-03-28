6126891d45442b535e3b280714625c01a4d0b08c
zainshah0347 <you@example.com>
2024-03-28 12:09:37 +0500

    Implement FastCash option and also the query that user can't get any extra amount in the ATM

--- main.ts
+++ main.ts
@@ -25,7 +25,7 @@ if (mainResult.pinCode === pinCode) {
       name: "multichoice",
       message: "Please select one of them",
       type: "list",
-      choices: ["Withdraw", "Check_balance"],
+      choices: ["Withdraw", "Check_balance", "Fast_Cash"],
     },
   ]);
 
@@ -39,13 +39,54 @@ if (mainResult.pinCode === pinCode) {
         type: "number",
       },
     ]);
+    
+    //Start this code user cannot get any extra amount
+    if(smallResult.amount <= myBalance){
+      myBalance -= smallResult.amount;
+      console.log("your remaining balance is ", myBalance);
 
-    myBalance -= smallResult.amount;
-    console.log("your remaining balance is ", myBalance);
+    }else{
+      console.log("Your Account Balance is insufficiant to make this Withdraw");
+    }
+    //End this code user cannot get any extra amount
+
+
+
   } else if (mediumResult.multichoice === "Check_balance") {
     console.log("Your balance is : ", myBalance);
   }
-}
-else{
-    console.log("Please enter a valid PinCode");
+  //Adding fast cash
+  else if (mediumResult.multichoice === "Fast_Cash") {
+    console.log("this is fast cash");
+
+    let fastCash = await inquirer.prompt([
+      {
+        name: "fastCash",
+        message: "Please select one amount given below",
+        type: "list",
+        choices: [1000, 2000, 3000, 5000],
+      },
+    ]);
+
+    if (fastCash.fastCash === 1000) {
+      myBalance -= fastCash.fastCash;
+      console.log("You can successfully Fast Cash ", 1000);
+      console.log("your remaining balance is :", myBalance);
+    } else if (fastCash.fastCash === 2000) {
+      myBalance -= fastCash.fastCash;
+      console.log("You can successfully Fast Cash ", 2000);
+      console.log("your remaining balance is :", myBalance);
+    } else if (fastCash.fastCash === 3000) {
+      myBalance -= fastCash.fastCash;
+      console.log("You can successfully Fast Cash ", 3000);
+      console.log("your remaining balance is :", myBalance);
+    } else if (fastCash.fastCash === 5000) {
+      myBalance -= fastCash.fastCash;
+      console.log("You can successfully Fast Cash ", 5000);
+      console.log("your remaining balance is :", myBalance);
+    }
+  }
+  //Ending fastCash
+} else {
+  console.log("Please enter a valid PinCode");
 }
